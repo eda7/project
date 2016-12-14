e44c05e824fd727f36bce7110d7045218bd41ef9
Bismarkowusukankam <qwekekankam@gmail.com>
2016-12-13 23:34:33 -1200

    login-footer.php is part of the health page.

---
+++ login-footer.php
@@ -0,0 +1,63 @@
+<footer class="footer">
+		        <div class="container">
+		            <nav class="pull-left">
+						<ul>
+							<li>
+								<a href="#">
+									
+								</a>
+							</li>
+							<li>
+								<a href="#">
+								   
+								</a>
+							</li>
+							<li>
+								<a href="#">
+								   
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									
+								</a>
+							</li>
+						</ul>
+		            </nav>
+		            <div class="copyright pull-right">
+		                &copy; <?php echo date('Y');?><i class=""></i><a href="#" target="_blank" id="ah"> Smart Health Systems</a>
+		            </div>
+		        </div>
+		    </footer>
+
+		</div>
+
+    </div>
+
+
+</body>
+	<!--   Core JS Files   -->
+	<script src="assets/login/js/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/login/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/login/js/material.min.js"></script>
+
+	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+	<script src="assets/login/js/nouislider.min.js" type="text/javascript"></script>
+
+	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+	<script src="assets/login/js/bootstrap-datepicker.js" type="text/javascript"></script>
+
+	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+	<script src="assets/login/js/material-kit.js" type="text/javascript"></script>
+
+	<script type="text/javascript">
+		$().ready(function(){
+			// the body of this function is in assets/material-kit.js
+			$(window).on('scroll', materialKit.checkScrollForTransparentNavbar);
+		});
+	</script>
+
+
+
+
+</html>
